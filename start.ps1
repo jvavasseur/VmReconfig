ad202b3abb10ab43251c0fd3d4beb64ab3b1fae6
Julien <sign.github@glop.info>
2020-04-21 05:41:05 -0800

    add init-vm + start

---
+++ start.ps1
@@ -0,0 +1,12 @@
+﻿$path = "C:\ProgramData\UiPath\Academy"
+$scripts = "C:\ProgramData\UiPath\Academy\Scripts"
+$repository = "https://github.com/jvavasseur/VmReconfig.git"
+
+$path.Split("\") | % { $fullpath = if( [io.path]::IsPathRooted($_) ) { Join-Path $_ -ChildPath "" } else { (New-Item $fullpath -Name $_ -ItemType Directory -Force ).FullName } }
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name Nuget -Force
+Install-Module posh-git -Scope CurrentUser -Force
+Import-Module posh-git
+#Add-PoshGitToProfile -AllHosts
+if (Test-Path $scripts) { Remove-Item $scripts -Force -Recurse }
+git clone $repository $scripts
