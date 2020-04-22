d7e0892855e9d3999a5f6da6b23015acb15ebaa0
Julien <sign.github@glop.info>
2020-04-22 06:12:47 -0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -2,14 +2,22 @@
 $scripts = "C:\ProgramData\UiPath\Academy\Scripts"
 $repository = "https://github.com/jvavasseur/VmReconfig.git"
 
+Write-Host "Add modules"
 $path.Split("\") | % { $fullpath = if( [io.path]::IsPathRooted($_) ) { Join-Path $_ -ChildPath "" } else { (New-Item $fullpath -Name $_ -ItemType Directory -Force ).FullName } }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name Nuget -Force
 Install-Module posh-git -Scope CurrentUser -Force
 Import-Module posh-git
 #Add-PoshGitToProfile -AllHosts
+
+Write-Host "Add Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Write-Host "Add git"
+choco install git -y --params "GitOnlyOnPath"
+
+Write-Host "Pull repository"
 if (Test-Path $scripts) { Remove-Item $scripts -Force -Recurse }
-git -C $scripts pull origin
+& 'C:\Program Files\Git\bin\git.exe' -C $scripts pull origin
 
-
+& 'C:\Program Files\Git\bin\git.exe' clone $repository $scripts
 
